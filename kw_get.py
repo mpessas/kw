e326cd9c15e818b2026717b906db8174960c7eae
Apostolos Mpessas <mpessas@gmail.com>
2010-08-07 20:15:33 +0300

    Function to setup arguments

--- kw_get.py
+++ kw_get.py
@@ -2,31 +2,56 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import argparse
+import optparse
+from kwerrors import InvalidCommandError
 
 
-__version__ = 0.1
+__version__ = '0.1'
 
-def arg_parser(prog_name):
-    parser = argparse.ArgumentParser(
-        description=u'A simple application to handle PIM data',
-        prog=prog_name
-    )
-    parser.add_argument(
-        u'cmd'
-        
-    )
-    parser.add_argument('--version',
-        action='version',
-        version='%(prog)s ' + __version__
-    )
-                        
+TASKS = ['get', 'set',]
 
+class ValueNotFoundError(Exception):
+    pass
+
+
+def run(options, args):
+    task = args.pop(0)
+    if task not in TASKS:
+        tasks = '[' + ', '.join(TASKS) + ']' 
+        msg = u'%s is not a valid command. Available commands are %s' % (task, tasks)
+        raise InvalidCommandError(msg)
+
+def setup_parser():
+    """Setup the parser.
+
+    Returns:
+        optparse.OptionParser with option configured.
+    """
+    usage = u'Usage: %prog cmd_name [options]'
+    parser = optparse.OptionParser(usage=usage,
+                                   version='%prog ' + __version__)
+    parser.add_option('-f', '--f', dest='folder', default=u'Passwords',
+                      help=u'Specify the folder you want to use')
+    parser.add_option('-k', '--key', dest='key', default='password',
+                      help=u'The key for this entry')
+    parser.add_option('-w', '--wallet', dest='wallet', default=u'personal',
+                      help=u'Specify the wallet to use')
+    return parser
 
 def main(argv=None):
     if argv is None:
         argv=sys.argv
-    parser = arg_parser(argv[0])
+    parser = setup_parser()
+    (options, args) = parser.parse_args()
+    try:
+        run(options, args)
+    except InvalidCommandError, e:
+        print e
+        return -1
+    except ValueNotFoundError, e:
+        print u'Specified entry not found.'
+        return -1
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
